829279ca5900a6ac4ac7cd3a2f5dd8654a24ff5d
David Santos <sandokas@gmail.com>
2021-03-18 00:02:52 +0000

    changed add to only be usable by representative officers. added remove command overload to include representative officers removing users from their regiments.

--- GCBot/Commands/InfoModule.cs
+++ GCBot/Commands/InfoModule.cs
@@ -29,13 +29,34 @@ namespace GCBot
             if (input.Contains("@"))
                 return ReplyAsync($"You can't insult that!");
 
-            return ReplyAsync($"You don't need my help for that. You're managing to insult {input} just fine on your own. Whatever that is...");
+            return ReplyAsync(GetInsult(input));
         }
 
         [Command("insult")]
         [Summary("Insults someone or something.")]
         public Task InsultAsync([Remainder][Summary("What you want to insult")] SocketUser user)
-        => ReplyAsync($"You don't need my help for that. You're managing to insult {user.Mention} just fine on your own.");
+        {
+
+            if (user.IsBot)
+                return ReplyAsync($"Insulting the Bots, really??");
+
+            return ReplyAsync(GetInsult(user.Mention));
+        }
+
+        private string GetInsult(string target)
+        {
+            var listInsults = new List<string>();
+            listInsults.Add("You don't need my help for that. You're managing to insult #target# just fine on your own. Whatever that is...");
+            listInsults.Add("#target# sounds like a boy!");
+            listInsults.Add("#target# doesn't press T...");
+            listInsults.Add("#target#'s mother is a fine lady!");
+            listInsults.Add("Can't be bothered, you do it...");
+            listInsults.Add("How much will you pay me, to do that?");
+            listInsults.Add("Yes AL. Whatever you say AL.");
+            var i = (new Random()).Next(listInsults.Count);
+
+            return listInsults[i].Replace("#target#", target);
+        }
 
         [Command("userinfo")]
         [Summary("Returns info about the current user, or the user parameter, if one passed.")]
@@ -51,6 +72,7 @@ namespace GCBot
 
         [Command("listroles")]
         [Summary("Returns list of available roles.")]
+        [Alias("list")]
         public async Task ListRolesAsync()
         {
             string rolesList = "";
@@ -63,6 +85,71 @@ namespace GCBot
             await ReplyAsync($"The following roles are available: {rolesList}.");
 
             return;
+        }
+
+        [Command("add")]
+        [Summary("Add someone to your regiment.")]
+        public async Task AddToRegimentAsync(
+            [Summary("The role name to list")]
+            SocketGuildUser user)
+        {
+            var requestingUser = Context.User;
+
+            #region Representative Officer permissions
+            var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == "Representative Officer");
+            if (role == null)
+            {
+                await ReplyAsync($"This command is temporarily disabled until Agentsvr knows what he's doing.");
+                return;
+            }
+            bool hasPermission = false;
+            foreach (var userRole in ((SocketGuildUser)requestingUser).Roles)
+            {
+                if (userRole.Id == role.Id)
+                    hasPermission = true;
+            }
+            if (!hasPermission)
+            {
+                await ReplyAsync($"Only Representative Officers can use this command.");
+                return;
+            }
+            #endregion
+
+            #region Load New Regiment
+            SocketRole regimentRole = null;
+            foreach (var userRole in ((SocketGuildUser)requestingUser).Roles)
+            {
+                foreach (var regiment in tokens.Tokens)
+                {
+                    if (userRole.Name == regiment.LongName)
+                        regimentRole = userRole;
+                }
+            }
+            if (regimentRole == null)
+            {
+                await ReplyAsync($"How can you be in a Representative Officer and not be in a recognized Regiment?");
+                return;
+            }
+            #endregion
+
+            #region Check if the user is already in a regiment
+            foreach (var userRole in user.Roles)
+            {
+                foreach (var regiment in tokens.Tokens)
+                {
+                    if (userRole.Name == regiment.LongName)
+                    {
+                        await ReplyAsync($"The user you're trying to add to your regiment is already in another regiment. Talk with the representative officer of {regiment.LongName}");
+                        return;
+                    }
+                }
+            }
+            #endregion
+
+            await user.AddRoleAsync(regimentRole);
+            await ReplyAsync($"Added {user.Nickname} to {regimentRole.Name}");
+            return;
+
         }
 
         [Command("listusers")]
@@ -113,7 +200,10 @@ namespace GCBot
                 foreach (var userRole in user.Roles)
                 {
                     if (userRole.Id == role.Id)
+                    {
                         usersWithRole.Add(user);
+                        break;
+                    }
                 }
             }
 
@@ -126,6 +216,7 @@ namespace GCBot
             await ReplyAsync($"{role.Name} has the following users: {usersList}");
         }
 
+        /*
         [Command("addrole")]
         [Summary("Gives you a role so you can show your affection towards your favorite GC Token.")]
         [Alias("support", "token")]
@@ -179,10 +270,10 @@ namespace GCBot
             result += $"{user.Username} now supports {requestedToken.LongName}.";
             await ReplyAsync(result);
         }
-
-        [Command("remrole")]
+        */
+        
+        [Command("remove")]
         [Summary("Removes your favorite GC Token role.")]
-        [Alias("remsupport", "remtoken")]
         public async Task RemoveRoleAsync()
         {
             var user = Context.User;
@@ -194,18 +285,83 @@ namespace GCBot
                     await (user as SocketGuildUser).RemoveRoleAsync(currentRole);
                     if (result != "")
                         result += "\r\n";
-                    result += $"{user.Username} no longer supports {currentRole.Name}.";
+                    result += $"{user.Username} no longer belongs to {currentRole.Name}.";
                 }
             }
             await ReplyAsync(result);
         }
-        [Command("remrole")]
+
+        [Command("remove")]
         [Summary("Removes your favorite GC Token role.")]
-        [Alias("remsupport", "remtoken")]
         public async Task RemoveRoleAsync(
             string toDiscard)
         {
             await RemoveRoleAsync();
+        }
+
+        [Command("remove")]
+        [Summary("Removes someone from your Regiment.")]
+        public async Task RemoveRoleAsync(
+            SocketGuildUser user)
+        {
+            var requestingUser = Context.User;
+
+            if (((SocketGuildUser)Context.User) == user)
+            {
+                await RemoveRoleAsync();
+                return;
+            }
+
+            #region Representative Officer permissions
+            var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == "Representative Officer");
+            if (role == null)
+            {
+                await ReplyAsync($"This command is temporarily disabled until Agentsvr knows what he's doing.");
+                return;
+            }
+            bool hasPermission = false;
+            foreach (var userRole in ((SocketGuildUser)requestingUser).Roles)
+            {
+                if (userRole.Id == role.Id)
+                    hasPermission = true;
+            }
+            if (!hasPermission)
+            {
+                await ReplyAsync($"Only Representative Officers can use this command.");
+                return;
+            }
+            #endregion
+
+            #region Load New Regiment
+            SocketRole regimentRole = null;
+            foreach (var userRole in ((SocketGuildUser)requestingUser).Roles)
+            {
+                foreach (var regiment in tokens.Tokens)
+                {
+                    if (userRole.Name == regiment.LongName)
+                        regimentRole = userRole;
+                }
+            }
+            if (regimentRole == null)
+            {
+                await ReplyAsync($"How can you be in a Representative Officer and not be in a recognized Regiment?");
+                return;
+            }
+            #endregion
+
+            foreach (var userRole in user.Roles)
+            {
+                if (userRole == regimentRole)
+                {
+                    await (user as SocketGuildUser).RemoveRoleAsync(regimentRole);
+                    await ReplyAsync($"Remove the regiment {regimentRole.Name} from {user.Nickname}.");
+                    return;
+                }
+            }
+
+            await ReplyAsync($"{user.Nickname} was not in your regiment {regimentRole.Name} anyway... you're welcome, I guess.");
+            return;
+
         }
     }
 }
