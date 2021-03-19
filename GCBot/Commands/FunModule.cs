64804d246e2f211d3a34da169fb28b907929e813
David Santos <sandokas@gmail.com>
2021-03-19 11:52:50 +0000

    split role commands from fun commmands added a check from me / you

---
+++ GCBot/Commands/FunModule.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace GCBot
+{
+
+    // Keep in mind your module **must** be public and inherit ModuleBase.
+    // If it isn't, it will not be discovered by AddModulesAsync!
+    public class FunModule : ModuleBase<SocketCommandContext>
+    {
+        private ListService lists;
+        private string botChannel = "bot-commands";
+        public FunModule(IServiceProvider services)
+        {
+            lists = services.GetRequiredService<ListService>();
+        }
+
+        [Command("hello")]
+        [Summary("Says hi to the bot.")]
+        public async Task HelloAsync()
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+            await ReplyAsync($"Oh. Hello, {Context.User.Mention}!");
+            return;
+        }
+
+        [Command("bye")]
+        [Summary("Say goodbye to the bot.")]
+        [Alias("goodbye")]
+        public async Task ByeAsync()
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+            await ReplyAsync($"Goodbye, {Context.User.Mention}. Have fun!");
+            return;
+        }
+
+        [Command("praise")]
+        [Summary("Praises someone or something.")]
+        public async Task PraiseAsync([Remainder][Summary("What you want to praise")] string input)
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+
+            if (input.Contains("@"))
+            {
+                await ReplyAsync($"You can't praise that!");
+                return;
+            }
+
+            if(input.ToLowerInvariant().Trim() == "you" || input.ToLowerInvariant().Trim() == "yourself")
+            {
+                await ReplyAsync($"Oh, thank you kind sir!");
+                return;
+            }
+
+            if (input.ToLowerInvariant().Trim() == "me")
+            {
+                await ReplyAsync($"Looking for some menaningless boost to self esteem are we?");
+                return;
+            }
+
+            await ReplyAsync(GetPraise(input, Context.User));
+            return;
+        }
+
+        [Command("praise")]
+        [Summary("Praises someone or something.")]
+        public async Task PraiseAsync([Remainder][Summary("What you want to praise")] SocketUser user)
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+
+            if (user.IsBot)
+            {
+                await ReplyAsync($"I know, we bots are the best, aren't we?");
+                return;
+            }
+
+            if (user == Context.User)
+            {
+                await ReplyAsync($"Looking for some menaningless boost to self esteem are we?");
+                return;
+            }
+
+            await ReplyAsync(GetPraise(user.Mention, Context.User));
+            return;
+        }
+        private string GetPraise(string target, SocketUser user)
+        {
+            var i = (new Random()).Next(lists.Praises.Count);
+
+            return lists.Praises[i].Replace("#target#", target).Replace("#user#", user.Mention);
+        }
+
+        [Command("insult")]
+        [Summary("Insults someone or something.")]
+        public async Task InsultAsync([Remainder][Summary("What you want to insult")] string input)
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+
+            if (input.Contains("@"))
+            {
+                await ReplyAsync($"You can't insult that!");
+                return;
+            }
+
+            if (input.ToLowerInvariant().Trim() == "you" || input.ToLowerInvariant().Trim() == "yourself")
+            {
+                await ReplyAsync($"We're back to insulting the bots, are we?");
+                return;
+            }
+
+            if (input.ToLowerInvariant().Trim() == "me")
+            {
+                await ReplyAsync($"Oh. Come on. You're not that bad! Here, have a cookie.");
+                return;
+            }
+
+
+            await ReplyAsync(GetInsult(input, Context.User));
+            return;
+        }
+
+        [Command("insult")]
+        [Summary("Insults someone or something.")]
+        public async Task InsultAsync([Remainder][Summary("What you want to insult")] SocketUser user)
+        {
+            if (Context.Channel.Name != botChannel)
+                return;
+
+            if (user.IsBot)
+            {
+                await ReplyAsync ($"Insulting the Bots, really??");
+                return;
+            }
+
+            if (user == Context.User)
+            {
+                await ReplyAsync($"Oh. Come on. You're not that bad! Here, have a cookie.");
+                return;
+            }
+
+            await ReplyAsync(GetInsult(user.Mention, Context.User));
+            return;
+        }
+
+        private string GetInsult(string target, SocketUser user)
+        {
+            var i = (new Random()).Next(lists.Insults.Count);
+
+            return lists.Insults[i].Replace("#target#", target).Replace("#user#", user.Mention);
+        }
+    }
+}
--- GCBot/Commands/RoleModule.cs
+++ GCBot/Commands/RoleModule.cs
@@ -1,134 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
-using Discord;
 using Discord.Commands;
-using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 
-namespace GCBot
+namespace GCBot.Commands
 {
-
-    // Keep in mind your module **must** be public and inherit ModuleBase.
-    // If it isn't, it will not be discovered by AddModulesAsync!
-    public class InfoModule : ModuleBase<SocketCommandContext>
+    public class RoleModule : ModuleBase<SocketCommandContext>
     {
         private ListService lists;
         private string botChannel = "bot-commands";
-        public InfoModule(IServiceProvider services)
+        public RoleModule(IServiceProvider services)
         {
             lists = services.GetRequiredService<ListService>();
         }
 
-        [Command("hello")]
-        [Summary("Says hi to the bot.")]
-        public async Task HelloAsync()
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-            await ReplyAsync($"Oh. Hello, {Context.User.Mention}!");
-            return;
-        }
-
-        [Command("bye")]
-        [Summary("Say goodbye to the bot.")]
-        [Alias("goodbye")]
-        public async Task ByeAsync()
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-            await ReplyAsync($"Goodbye, {Context.User.Mention}. Have fun!");
-            return;
-        }
-
-        [Command("praise")]
-        [Summary("Praises someone or something.")]
-        public async Task PraisetAsync([Remainder][Summary("What you want to praise")] string input)
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-
-            if (input.Contains("@"))
-            {
-                await ReplyAsync($"You can't praise that!");
-                return;
-            }
-
-
-            await ReplyAsync(GetPraise(input, Context.User));
-            return;
-        }
-
-        [Command("praise")]
-        [Summary("Praises someone or something.")]
-        public async Task PraiseAsync([Remainder][Summary("What you want to praise")] SocketUser user)
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-
-            if (user.IsBot)
-            {
-                await ReplyAsync($"I know, we bots are the best, aren't we?");
-                return;
-            }
-
-            await ReplyAsync(GetPraise(user.Mention, Context.User));
-            return;
-        }
-        private string GetPraise(string target, SocketUser user)
-        {
-            var i = (new Random()).Next(lists.Praises.Count);
-
-            return lists.Praises[i].Replace("#target#", target).Replace("#user#", user.Mention);
-        }
-
-        [Command("insult")]
-        [Summary("Insults someone or something.")]
-        public async Task InsultAsync([Remainder][Summary("What you want to insult")] string input)
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-
-            if (input.Contains("@"))
-            {
-                await ReplyAsync($"You can't insult that!");
-                return;
-            }
-
-            
-            await ReplyAsync(GetInsult(input, Context.User));
-            return;
-        }
-
-        [Command("insult")]
-        [Summary("Insults someone or something.")]
-        public async Task InsultAsync([Remainder][Summary("What you want to insult")] SocketUser user)
-        {
-            if (Context.Channel.Name != botChannel)
-                return;
-
-            if (user.IsBot)
-            {
-                await ReplyAsync ($"Insulting the Bots, really??");
-                return;
-            }
-
-            await ReplyAsync(GetInsult(user.Mention, Context.User));
-            return;
-        }
-
-        private string GetInsult(string target, SocketUser user)
-        {
-            var i = (new Random()).Next(lists.Insults.Count);
-
-            return lists.Insults[i].Replace("#target#", target).Replace("#user#", user.Mention);
-        }
-
-        [Command("listroles")]
-        [Summary("Returns list of available roles.")]
+        [Command("listregiments")]
+        [Summary("Returns list of available regiments.")]
         [Alias("list")]
         public async Task ListRolesAsync()
         {
@@ -142,7 +32,7 @@ namespace GCBot
                     rolesList += ", ";
                 rolesList += token.ShortName;
             }
-            await ReplyAsync($"The following roles are available: {rolesList}.");
+            await ReplyAsync($"The following regiments are available: {rolesList}.");
 
             return;
         }
@@ -216,7 +106,7 @@ namespace GCBot
         }
 
         [Command("listusers")]
-        [Summary("Returns list of users with given role.")]
+        [Summary("Returns list of users in a given Regiment.")]
         public async Task ListUsersByRoleAsync(
             [Remainder][Summary("The role name to list")]
             string roleName = null)
@@ -282,9 +172,9 @@ namespace GCBot
             await ReplyAsync($"{role.Name} has the following users: {usersList}");
         }
 
-        [Command("remove")]
-        [Summary("Removes your favorite GC Token role.")]
-        [Alias("leave")]
+        [Command("leave")]
+        [Summary("Leave your regiment.")]
+        [Alias("remove")]
         public async Task RemoveRoleAsync()
         {
             if (Context.Channel.Name != botChannel)
@@ -305,9 +195,9 @@ namespace GCBot
             await ReplyAsync(result);
         }
 
-        [Command("remove")]
-        [Summary("Removes your favorite GC Token role.")]
-        [Alias("leave")]
+        [Command("leave")]
+        [Summary("Leave your regiment.")]
+        [Alias("remove")]
         public async Task RemoveRoleAsync(
             string toDiscard)
         {
@@ -381,5 +271,6 @@ namespace GCBot
             return;
 
         }
+
     }
 }
